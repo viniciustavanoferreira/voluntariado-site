edacded490462dd47c4f5fb5589301b947e2b427
Leo <macedobeiramar@hotmail.com>
2023-08-22 17:59:58 -0300

    PARTE III - Cores e logo

--- index.php
+++ index.php
@@ -22,6 +22,11 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
+    <!-- Font Styles -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
 
 </head>
 
@@ -32,13 +37,17 @@
     <!-- Header Finsih -->
 
     <!-- nav bar start -->
-
-    <nav class="navbar navbar-expand-lg navbar-white fixed-top py-lg-0 px-lg-5 wow fadeIn"data-wow-delay="0.1s" style="background-color: var(--MainColor);">
+<main class="root">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-lg-0 px-lg-5 wow fadeIn"data-wow-delay="0.1s">
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="logo">
+                        <img src="./src/img/logo.jpg" alt="">
+                    </div>
             <div class="navbar-nav mx-auto p-4 p-lg-0">
+            
                 <a href="index.php" class="nav-item nav-link active text-black">Home</a>
                 <a href="./php/quem-somos.php" class="nav-item nav-link text-black">Quem Somos</a>
                 <a href="./php/contato.php" class="nav-item nav-link text-black">Contato</a>
@@ -147,6 +156,7 @@
     <!-- About End -->
 
     <div class="hero-subtitle text-black text-center mt-3 mt-lg-4">
+        
 
     </div>
 
@@ -210,6 +220,7 @@
     <?php
     include("./php/footer.php")
     ?>
+</main>
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top custom"><i class="bi bi-arrow-up"></i></a>
