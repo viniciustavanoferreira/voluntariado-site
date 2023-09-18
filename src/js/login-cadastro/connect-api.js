c91536b2381ab2b744a106e04abe653100efab77
LuisTakeo <luistakeo@hotmail.com>
2023-09-18 16:23:06 -0300

    feat: add JS

---
+++ src/js/login-cadastro/connect-api.js
@@ -0,0 +1,55 @@
+async function login(email, password) {
+  const requestOptions = {
+    method: "POST",
+    redirect: "follow",
+  };
+  const conexao = await fetch(
+    `https://sistema-voluntariado-backend.onrender.com/v1/api/usuario/login/email/${email}/password/${password}`,
+    requestOptions
+  );
+  if (!conexao.ok) {
+    throw new Error("Não foi possível conexão. Tente novamente");
+  }
+
+  const conexaoTexto = await conexao.json();
+  return conexaoTexto;
+  // .then((response) => response.text())
+  // .then((result) => console.log(result))
+  // .catch((error) => console.log("error", error));
+}
+
+async function register(cadastro) {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: cadastro.nome,
+      dataNascimento: cadastro.dataNascimento,
+      email: cadastro.email,
+      telefone: cadastro.telefone,
+      cep: cadastro.cep,
+      endereco: cadastro.endereco,
+      usuario: cadastro.usuario,
+      senha: cadastro.senha,
+      perfil: cadastro.perfil,
+    }),
+    redirect: "follow",
+  };
+  const conexao = await fetch(
+    `https://sistema-voluntariado-backend.onrender.com/v1/api/usuario/${cadastro.perfil}`,
+    requestOptions
+  );
+  if (!conexao.ok) {
+    throw new Error("Não foi possível conexão. Tente novamente");
+  }
+
+  const conexaoResposta = await conexao.json();
+  return conexaoResposta;
+}
+
+export const connection = {
+  login,
+  register,
+};
--- src/views/login-cadastro.php
+++ src/views/login-cadastro.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     <link rel="stylesheet" href="../css/login-cadastro/styles.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <div class="content first-content">
@@ -17,31 +18,32 @@
                 <p class="description description-primary">.......</p>
                 <p class="description description-primary">É novo aqui?</p>
                 <button id="signin" class="btn btn-primary">Cadastrar</button>
-            </div>    
+            </div>
             <div class="second-column">
                 <h2 class="title title-second">Acesse aqui</h2>
- 
+
                 <p class="description description-second">Faça login da sua conta</p>
                 <form class="form">
-                                       
+
                     <label class="label-input" for="">
                         <i class="far fa-envelope icon-modify"></i>
-                        <input type="email" placeholder="Email">
+                        <input name="txEmail" type="email" placeholder="Email">
                     </label>
-                    
+
                     <label class="label-input" for="">
                         <i class="fas fa-lock icon-modify"></i>
-                        <input type="password" placeholder="Password">
+                        <input name="txPassword" type="password" placeholder="Password">
                     </label>
-                    
-                    
-                    <button class="btn btn-second" onclick="login()">Entrar</button>     
-                    <a class="password" href="#">Esqueceu sua senha?</a>   
+
+
+                    <button id="signup" class="btn btn-second">Entrar</button>
+                    <a class="password" href="#">Esqueceu sua senha?</a>
                 </form>
             </div><!-- second column -->
         </div><!-- first content -->
 
     </div>
-    <script src="../js/login-cadastro/main.js"></script>
+    <script type="module" src="../js/login-cadastro/login-main.js"></script>
 </body>
+
 </html>
