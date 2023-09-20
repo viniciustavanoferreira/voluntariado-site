6541d00c70ccd67e76f67f4e9691df6e3a537952
LuisTakeo <luistakeo@hotmail.com>
2023-09-20 20:41:15 -0300

    Commit do código comentado como atividade da aula de DS-II

--- src/js/login-cadastro/connect-api.js
+++ src/js/login-cadastro/connect-api.js
@@ -1,24 +1,28 @@
+// função assincrona para requisição POST de login para a API do nosso backend
 async function login(email, password) {
+  // tipo de requisição que vai ser feita
   const requestOptions = {
     method: "POST",
     redirect: "follow",
   };
+  // constante que vai guardar a resposta da requisição. O await é utilizado junto com o async, aonde só vai ser atribuido o valor quando tiver resposta da API
   const conexao = await fetch(
     `https://sistema-voluntariado-backend.onrender.com/v1/api/usuario/login/email/${email}/password/${password}`,
     requestOptions
   );
+  // verificar se a conexão foi feita com sucesso. Caso não seja, será retornado um erro
   if (!conexao.ok) {
     throw new Error("Não foi possível conexão. Tente novamente");
   }
-
+  // constante que será armazenada o arquivo .json que vem da API como um objeto.
   const conexaoTexto = await conexao.json();
+  // retorno da constante com o json transformado em objeto
   return conexaoTexto;
-  // .then((response) => response.text())
-  // .then((result) => console.log(result))
-  // .catch((error) => console.log("error", error));
 }
 
+// função assincrona para requisição POST  para a API do nosso backend
 async function register(cadastro) {
+  // tipo de requisição que vai ser feita, aonde terá o body com os campos do cadastro
   const requestOptions = {
     method: "POST",
     headers: {
@@ -37,18 +41,22 @@ async function register(cadastro) {
     }),
     redirect: "follow",
   };
+  // constante que vai guardar a resposta da requisição. O await é utilizado junto com o async, aonde só vai ser atribuido o valor quando tiver resposta da API
   const conexao = await fetch(
     `https://sistema-voluntariado-backend.onrender.com/v1/api/usuario/${cadastro.perfil}`,
     requestOptions
   );
+  // verificar se a conexão foi feita com sucesso. Caso não seja, será retornado um erro
   if (!conexao.ok) {
     throw new Error("Não foi possível conexão. Tente novamente");
   }
-
+  // constante que será armazenada o arquivo .json que vem da API.
   const conexaoResposta = await conexao.json();
+  // retorno da constante com o json transformado em objeto
   return conexaoResposta;
 }
 
+// exportando constante que terá as funções de login e register para uso do js que manipulará a pagina login-main 
 export const connectionLoginRegister = {
   login,
   register,
--- src/js/perfil-voluntario/connect-api.js
+++ src/js/perfil-voluntario/connect-api.js
@@ -1,16 +1,22 @@
+// função assincrona para requisição GET de login para a API do nosso backend
 async function getServices(email) {
+  // tipo de requisição que vai ser feita 
   const requestOptions = {
     method: "GET",
     redirect: "follow",
   };
+  // constante que vai guardar a resposta da requisição. O await é utilizado junto com o async, aonde só vai ser atribuido o valor quando tiver resposta da API
   const conexao = await fetch(
     `https://sistema-voluntariado-backend.onrender.com/v1/api/servico/voluntario/${email}`,
     requestOptions
   );
+  // verificar se a conexão foi feita com sucesso. Caso não seja, será retornado um erro
   if (!conexao.ok) {
     throw new Error("Não foi possível conexão. Tente novamente");
   }
+  // constante que será armazenada o arquivo .json que vem da API como um objeto.
   const conexaoTexto = await conexao.json();
+  // retorno da constante com o json transformado em objeto
   return conexaoTexto;
 }
 
@@ -18,6 +24,7 @@ async function getServices(email) {
 //   console.log("teste");
 // }
 
+// exportando constante que terá as funções de conexão de serviços do Voluntário para uso do js que manipulará a pagina perfil-voluntario 
 export const connectServicesV = {
   getServices,
   // teste,
