a45ba14ce64e3202198e4de7a0ed43f11bd679fd
Leo <macedobeiramar@hotmail.com>
2023-10-02 18:11:14 -0300

    feature/final-design:service

--- php/perfil-idoso.php
+++ php/perfil-idoso.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Perfil do Usuário</title>
+    <link rel="icon" type="image/png" href="./src/img/logosemfundo.png">
+
+        <!-- Principal CSS -->
     <link rel="stylesheet" href="../pgprincipal.css">
     
-    
+    <!-- Customized CSS -->
+
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+
+
 
     <!-- Icon Font Stylesheet -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
@@ -20,151 +29,173 @@
 
 <body>
 
-    <header class="header">
-        <h1 class="header__title">Seja bem vindo, gente boa!</h1>
-        <img src="../src/img/logo.jpg" alt="logo-site">
+<header class="header h-100" style="background-color:blanchedalmond;">
+       
+        <img src="../src/img/logosemfundo.png" alt="logo-site">
     </header>
 
-    <aside class="sidebar border">
-        <header class="sidebar-header">
+
+
+<div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-3 col-lg-2 d-md-block sidebar" style="padding-top: 120px; background-color:#DCDCDC">
+    <header class="sidebar-header">
             <img class="logo-img" src="../src/img/et.png" alt="Foto do usuário">
         </header>
+      <!-- Conteúdo da barra lateral aqui -->
 
-        <nav>
-            <div class="sidebar__navgation">
-                <button id ="mostrar-perfil">
+      
+      <div class="sidebar__navgation">
+                <button id="btnmostrar-perfil">
                     <span>
-                        <i class="material-symbols-outlined">Home</i>
-                        <span>Profile</span>
-                    </span>
-
-                </button>
-                
-
-                <button>
-                    <span>
-                        <i class="material-symbols-outlined">tag</i>
+                        <i class="bi bi-house-door"></i>
                         <span>Perfil</span>
                     </span>
-                </button>
-            </div>
 
-            <button>
-                <span>
-                    <i class="material-symbols-outlined">tag</i>
-                    <span>Sair</span>
-                </span>
-            </button>
-        </nav>
+                </button>
+
+                <button id="btnServico">
+                    <span>
+                        <i class="bi bi-clock-history"></i>
+                        <span>Serviços</span>
+                    </span>
+                </button>
+
+
+                <button id="btnConfig">
+                    <span>
+                        <i class="bi bi-tools"></i>
+                        <span>Configurações</span>
+                    </span>
+                </button>
+
+                <button id="btnPriv">
+                    <span>
+                        <i class="bi bi-lock"></i>
+                        <span>Privacidade e Senha</span>
+                    </span>
+                </button>
+
+                <div class="button-container">
+                <button id="btnVoltar">
+                    <span>
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Voltar</span>
+                    </span>
+                </button>
+
+
+                <button id="btnSair">
+                    <span>
+                    <i class="bi bi-door-closed"></i>
+                        <span>Logout</span>
+                    </span>
+                </button>
+                    
+                </div>
+    </nav>
+    
+  </div>
+</div>
+        
     </aside>
 
 
-    <!--menu mobile-->
 
-    <button class="button-mobile" onclick="toggleMenu()">
-        <i class="material-symbols-outlined">menu</i>
-        <span>Menu</span>
-    </button>
+    
+<!-- Formulario de Serviço -->
 
-    <nav class="menu-mobile" id="menu-mobile">
-        <button class="button-close" onclick="toggleMenu()">
-            <span>
-                <i class="material-symbols-outlined">close</i>
-            </span>
-        </button>
+<div class="card__form hidden form-control-lg form-control-sm" id="registration-form">
+        <form>
+            <div class="form-icon">
+                <span><i class="icon icon-user"></i></span>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="destino-form" placeholder="Destino" required oninvalid="this.setCustomValidity('Por favor, preencha este campo.')">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="ordem-descricao-form" placeholder="Descrição" required oninvalid="this.setCustomValidity('Por favor, preencha este campo.')">
+            </div>
+            <div class="form-group">
+        <label for="serviceSelect">Selecione o Serviço:</label>
+        <select id="serviceSelect-form" class="form-control" style="background-color:#fdf5eb;">
+            <option value="1">Lâmpadas</option>
+            <option value="2">Pequenos Reparos</option>
+            <option value="3">Passeio</option>
+            <option value="4">Outros Serviços</option>
+        </select>
+    </div>
+            <div class="form-group">
+            <button type="button" class="btn btn-block create-account">Salvar</button>
+            </div>
+        </form>
+        <div id="validationMessage" style="color: red;"></div>
+    </div>
+    
+     <!-- Formulário de exibição e edição de perfil -->
 
-        <button>
-            <span>
-                <i class="material-symbols-outlined"> Home </i>
-                <span class="mobile-text">Home</span>
-            </span>
-        </button>
-
-        <button>
-            <span>
-                <i class="material-symbols-outlined"> tag </i>
-                <span class="mobile-text">Sair</span>
-            </span>
-        </button>
-    </nav>
-
-    <div class="teste">
-    <div id="registration-form" class="hidden" style="display: flex;
-    justify-content: center;">
-
-                    <form>
-                        <div class="form-icon">
-                            <span><i class="icon icon-user"></i></span>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="username" placeholder="Usuário">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="Senha">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="idSenha">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="email" placeholder="Email">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="phone-number" placeholder="Endereço">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="birth-date" placeholder="Nascimento">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="Bloco">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="Numero">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="Visitante?">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="Disponibilidade">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="idUsuarioIdoso">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="Condicão de Saude">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control item" id="password" placeholder="Assitencia Requirida">
-                        </div>
-                        <div class="form-group">
-                            <button type="button" class="btn btn-block create-account">Salvar</button>
-                        </div>
-            
-                    </form>
+     <div class="card__form hidden form-control-lg form-control-sm" id="registration-form-perfil">
+        <form>
+            <div class="form-icon">
+                <span><img class="logo-img" src="../src/img/et.png" alt="Foto do usuário"></span>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="usuario-perfil" placeholder="Usuário" readonly>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="nome-perfil" placeholder="Nome" readonly>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="email-perfil" placeholder="E-mail" readonly>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="endereço-perfil" placeholder="Endereço" readonly>
+            </div>
+            <div class="form-group">
+            <div class="form-group">
+                <input type="text" class="form-control item" id="número-perfil" placeholder="Número" readonly>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="bairro-perfil" placeholder="Bairro" readonly>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="cidade-perfil" placeholder="Cidade" readonly>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="estado-perfil" placeholder="Estado" readonly>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="idade-perfil" placeholder="Idade" readonly>
             </div>
             
+        </form>
     </div>
 
-    <script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const mainContent = document.querySelector(".main");
-
-        const mostrarPerfilButton = document.getElementById("mostrar-perfil");
-        const registrationForm = document.getElementById("registration-form");
-        const saveButton = document.querySelector("#registration-form button.create-account");
-
-        mostrarPerfilButton.addEventListener("click", function () {
+    <main class="main" id="hidden-main" style="display:none;">
+        <!-- Criar servico -->
+    <h2 class="main-title"  id="criar-servico-title">
+            <span> <i class="bi bi-pen"></i>
+            Criar Serviço</h2>
+            <span> 
+    
+            <div class="main__servicos__card" id="make-service" >
+                <div class="card__text">
+                    <h4>Ação</h4>
+                    <p>Cadastre seu serviço</p>
+                </div>
+                
+                <div class="card__button" id ="btnFazerCard" style="justify-content: center;">
+                    <a href="#">Fazer</a>
+                </div>
+                
+            </div>
             
-            registrationForm.classList.toggle("hidden");
-            
-            mainContent.style.display = "none";
-        });
-    });
+            <!-- Histórico -->
+        <h2 class="main-title" id="historico-title">
+            <span> <i class="bi bi-book"></i>
+            Histórico</h2>
+            <span>
 
-
-    </script>
-
-    <main class="main">
-        <h2 class="main-title">Serviços Disponiveis</h2>
 
         <section class="main__servicos">
             <div class="main__servicos__card">
@@ -172,12 +203,17 @@
                     <h4>Eustáquio</h4>
                     <p>Serviço de Troca de lampada</p>
                 </div>
-                <div class="card__button">
+                <div class= "card__button-group">
+                <div class="card__button" id="btnAceitarServi">
                     <a href="#">Aceitar</a>
                 </div>
-                <div class="card__button">
-                    <a href="#">Rejeitar</a>
+                <div class="card__button" id="btnEditaServ">
+                    <a href="#">Editar</a>
                 </div>
+                <div class="card__button" id="btnEXcluirServ" >
+                    <a href="#">Excluir</a>
+                </div>
+                 </div>
             </div>
 
             <div class="main__servicos__card">
@@ -185,12 +221,17 @@
                     <h4>Ana Francisca</h4>
                     <p>Serviço de Bate papo</p>
                 </div>
-                <div class="card__button">
+                <div class= "card__button-group">
+                <div class="card__button" id="btnAceitarServi">
                     <a href="#">Aceitar</a>
                 </div>
-                <div class="card__button">
-                    <a href="#">Rejeitar</a>
+                <div class="card__button" id="btnEditarServi">
+                    <a href="#">Editar</a>
                 </div>
+                <div class="card__button" id="btnEXcluirServ" >
+                    <a href="#">Excluir</a>
+                </div>
+                 </div>
             </div>
 
             <div class="main__servicos__card">
@@ -198,12 +239,17 @@
                     <h4>Ana Francisca</h4>
                     <p>Serviço de Bate papo</p>
                 </div>
-                <div class="card__button">
+                <div class= "card__button-group">
+                <div class="card__button" id="btnAceitarServi">
                     <a href="#">Aceitar</a>
                 </div>
-                <div class="card__button">
-                    <a href="#">Rejeitar</a>
+                <div class="card__button" id="btnEditarServi">
+                    <a href="#">Editar</a>
                 </div>
+                <div class="card__button" id="btnEXcluirServ" >
+                    <a href="#">Excluir</a>
+                </div>
+                 </div>
             </div>
 
             <div class="main__servicos__card">
@@ -211,18 +257,52 @@
                     <h4>Ana Francisca</h4>
                     <p>Serviço de Bate papo</p>
                 </div>
-                <div class="card__button">
+                <div class= "card__button-group">
+                <div class="card__button" id="btnAceitarServi">
                     <a href="#">Aceitar</a>
                 </div>
-                <div class="card__button">
-                    <a href="#">Rejeitar</a>
+                <div class="card__button" id="btnEditarServi">
+                    <a href="#">Editar</a>
                 </div>
+                <div class="card__button" id="btnEXcluirServ" >
+                    <a href="#">Excluir</a>
+                </div>
+                 </div>
             </div>
         </section>
 
+        <!-- Edição de Servicos -->
+
+        <div class="card__form hidden form-control-lg form-control-sm" id="registration-form" style="display:none;">
+        <form id="serviceForm">
+            <div class="form-icon">
+                <span><i class="icon icon-user"></i></span>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="destino-form" placeholder="Destino" required oninvalid="this.setCustomValidity('Por favor, preencha este campo.')">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="ordem-descricao-form" placeholder="Descrição" required oninvalid="this.setCustomValidity('Por favor, preencha este campo.')">
+            </div>
+            <div class="form-group">
+        <label for="serviceSelect">Selecione o Serviço:</label>
+        <select id="serviceSelect-form" class="form-control" style="background-color:#fdf5eb;">
+            <option value="1">Lâmpadas</option>
+            <option value="2">Pequenos Reparos</option>
+            <option value="3">Passeio</option>
+            <option value="4">Outros Serviços</option>
+        </select>
+    </div>
+            <div class="form-group">
+            <button type="button" class="btn btn-block create-account">Salvar</button>
+            </div>
+        </form>
+        <div id="validationMessage" style="color: red;"></div>
+    </div>
+
     </main>
 
-    <script type="module" src="../src/js/perfil-voluntario/pagina-perfil.js"></script>
+    <script type="module" src="../src/js/perfil.js"></script>
 </body>
 
 </html>
