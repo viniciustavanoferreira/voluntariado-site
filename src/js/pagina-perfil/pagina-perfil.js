a19649076480973f2f3ede73499d4b8498d50339
Leo <macedobeiramar@hotmail.com>
2023-10-10 18:18:10 -0300

    fix/bugs:buttons&design

--- src/js/pagina-perfil/pagina-perfil.js
+++ src/js/pagina-perfil/pagina-perfil.js
@@ -26,3 +26,52 @@ $(document).ready(function () {
           $('#wrapper').toggleClass('toggled');
     });  
   });
+
+  const btnMostrarPerfil = document.querySelector('#btnMostrar-perfil-link');
+  const perfilContainer = document.querySelector('#registration-form-perfil');
+  const btnServicos = document.querySelector('#btnMostrar-servicos-link');
+  const mainTitleCriar = document.querySelector('#criar-servico-title');
+  const mainTitleHistorico = document.querySelector('#historico-title');
+  const mainCriarServicoCard = document.querySelector('#make-service');
+  const mostrarServicoCard = document.querySelector('#mostrar-servico-card');
+  const btnEditarPerfil = document.querySelector('#btnEditar-perfil-link');
+  const btnEditarSenha = document.querySelector('#btnEditar-senha-link');
+  const editarSenhaContainer = document.querySelector('#form-senha-usuario');
+
+  
+  btnMostrarPerfil.addEventListener('click', () => {
+    perfilContainer.className = "card__form_perfil";
+    
+  
+  });
+
+  btnServicos.addEventListener('click', () => {
+    mainTitleCriar.className = "main-title";
+    mainTitleHistorico.className = "main-title";
+    mainCriarServicoCard.className = "main__servicos__card";
+    mostrarServicoCard.className += "main__servicos_card";
+    perfilContainer.className += "esconder";
+    
+  });
+
+  btnEditarPerfil.addEventListener('click',() =>{
+// tem que criar
+  });
+
+  btnEditarSenha.addEventListener('click',() =>{
+    editarSenhaContainer.className += "card__form";
+    mainTitleCriar.className = "esconder";
+    mainTitleHistorico.className = "esconder";
+    mainCriarServicoCard.className = "esconder";
+    mostrarServicoCard.className = "esconder";
+    perfilContainer.className = "esconder";
+
+
+
+  });
+
+
+
+ 
+ 
+  
