eed00298a6d7a8278f4ef2bb1d880904fa84820c
Naats.lima <nathaliaxlimma1@gmail.com>
2023-11-01 22:21:17 -0300

    feat: implementando nova pagina de cadastro

--- src/js/login-cadastro/cadastro-main.js
+++ src/js/login-cadastro/cadastro-main.js
@@ -21,15 +21,41 @@ const opcoes = document.querySelectorAll("[name=drone]");
 const box_botao = document.querySelector(".box-botao");
 const texto_antecedente = document.querySelector(".texto_crime");
 
+const formSignin = document.querySelector('#signin')
+const formSignup = document.querySelector('#signup')
+const btnColor = document.querySelector('.btnColor')
+
+document.querySelector('.btnSignin')
+  .addEventListener('click', () => {
+    formSignin.style.left = "25px"
+    formSignup.style.left = "450px"
+    btnColor.style.left = "0px"
+})
+
+document.querySelector('.btnSignup')
+  .addEventListener('click', () => {
+    formSignin.style.left = "-450px"
+    formSignup.style.left = "25px"
+    btnColor.style.left = "110px"
+})
+
+console.log("teste")
+
+
+console.log(texto_antecedente)
+console.log(box_botao)
+
+// texto_antecedente.className = "texto_crime hide";
+
 opcoes.forEach((opcao) => {
   opcao.addEventListener("click", () => {
     if (opcao.value === "idoso") {
       box_botao.className = "box-botao hide";
-      texto_antecedente.className = ".texto_crime hide";
+      texto_antecedente.className = "texto_crime hide";
     }
     if (opcao.value === "voluntario") {
       box_botao.className = "box-botao";
-      texto_antecedente.className = ".texto_crime";
+      texto_antecedente.className = "texto_crime";
     }
   });
 });
