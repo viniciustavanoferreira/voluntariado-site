299f5b7172c08e5ed502d6a1231ca35afba4fdeb
Leo <macedobeiramar@hotmail.com>
2023-12-05 00:38:20 -0300

    add:layout-quemsomos

--- php/quemsomos.php
+++ php/quemsomos.php
@@ -1,133 +1,158 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quem Somos</title>
     <link rel="icon" type="image/png" href="./src/img/logosemfundo.png">
-<!-- Principal CSS -->
+    <!-- Principal CSS -->
 
-<link rel="stylesheet" href="../src/css/quem-somos.css" />
+    <link rel="stylesheet" href="../src/css/quem-somos.css" />
 
 
 
-<!-- Font Styles -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- Font Styles -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
+
 <body>
 
-<!-- nav -->
-
-    
-
-<div class="container">
-    <div class="conta">
-        <div class="bl-main" id="bl-main">
-            <section>
-                <div class="bl-box">
-                    <h2>Nossa História</h2>
-                </div>
-
-                <div class="bl-content">
-
-                    <p>Nossa jornada começou com a simples ideia de que podemos fazer a diferença na vida dos idosos, que muitas vezes enfrentam a solidão e desafios diários. Fundada por um grupo de apaixonados por causas sociais, nossa plataforma nasceu da crença de que cada ato de bondade conta e pode iluminar o dia de alguém. Juntos, estabelecemos o compromisso de criar um ambiente onde o voluntariado não seja apenas uma tarefa, mas uma conexão sincera.</p>
-                </div><span class="bl-icon bl-icon-close">Fechar</span>
-            </section>
-
-            <section>
-                <div class="bl-box">
-                    <h2>Missão</h2>
-                </div>
-
-                <div class="bl-content">
-
-                <p>Nossa missão é dupla: oferecer uma mão amiga aos idosos que precisam de apoio e enriquecer a vida daqueles que desejam contribuir com seu tempo e cuidado. Acreditamos que todos têm algo especial para compartilhar, e ao conectar pessoas dispostas a ajudar com idosos que anseiam por companhia, estamos construindo uma comunidade baseada no respeito, compaixão e empatia.</p>
-   
-                </div><span class="bl-icon bl-icon-close">Fechar</span>
-            </section>
-
-            <section>
-                <div class="bl-box">
-                    <h2>Valores</h2>
-                </div>
-
-                <div class="bl-content">
-
-                    <p>Somos uma ponte entre pessoas que desejam fazer a diferença e idosos que precisam de apoio. Facilitamos a conexão entre voluntários e idosos, levando em consideração seus interesses, habilidades e disponibilidade. Nossa plataforma é projetada para tornar o processo de voluntariado simples e gratificante. Desde visitas regulares para um bate-papo amigável até auxílio em tarefas do dia a dia, cada interação é uma oportunidade de criar memórias significativas.</p>
-                </div><span class="bl-icon bl-icon-close">Fechar</span>
-            </section>
-
-            <section id="bl-work-section">
-                <div class="bl-box">
-                    <h2>Quem Somos</h2>
-                </div>
-
-                <div class="bl-content">
-                    <h2>Integrantes</h2>
-
-    
-
-                    <ul id="bl-work-items" style=" position: absolute;
-    display: flex;
-    top: 200px;">
-                        <li data-panel="panel-1">
-                            <a href="#"><img src="../src/img/nat2.jpg"></a>
-                            <h2> Damião </h2>
-                            <h3> Documentação & Frontend<h3>
-                        </li>
-
-                        <li data-panel="panel-2">
-                            <a href="#"><img src="../src/img/leo2.jpg"></a>
-                            <h2> Leandro </h2>
-                            <h3> Design, Frontend & Gerenciamento de Dados<h3>
-                        </li>
-
-                        <li data-panel="panel-3">
-                            <a href="#"><img src="images/5.jpg"></a>
-                        </li>
-                        <h2> Luis </h2>
-                        <h3> Frontend <h3>
+    <!-- nav -->
 
 
-                        <li data-panel="panel-4">
-                            <a href="#"><img src="../src/img/nat1.jpeg"></a>
-                            <h2> Nathalia </h2>
-                            <h3> Documentação & Frontend <h3>
-                        </li>
 
-                        <li data-panel="panel-5">
-                        <a href="#"><img src="../src/img/vinicius1.jpg"></a>
-                        <h2> Vinicius </h2>
-                        <h3> Backend & Gerencimaneto de Dados<h3>
-                        </li>
-                        
-                    </ul>
-                </div><span class="bl-icon bl-icon-close">Fechar</span>
-            </section>
+    <div class="container">
+        <div class="conta">
+            <div class="bl-main" id="bl-main">
+                <section>
+                    <div class="bl-box">
+                        <h2>Nossa História</h2>
+                    </div>
 
-            <section>
-                <div class="bl-box">
-                    <h2> <a href="../index.php">Voltar a Pagina Principal</a></h2>
-                </div>
+                    <div class="bl-content">
 
-                
-            </section>
+                        <p>Nossa jornada começou com a simples ideia de que podemos fazer a diferença na vida dos idosos, que muitas vezes enfrentam a solidão e desafios diários. Fundada por um grupo de apaixonados por causas sociais, nossa plataforma nasceu da crença de que cada ato de bondade conta e pode iluminar o dia de alguém. Juntos, estabelecemos o compromisso de criar um ambiente onde o voluntariado não seja apenas uma tarefa, mas uma conexão sincera.</p>
+                    </div><span class="bl-icon bl-icon-close">Fechar</span>
+                </section>
+
+                <section>
+                    <div class="bl-box">
+                        <h2>Missão</h2>
+                    </div>
+
+                    <div class="bl-content">
+
+                        <p>Nossa missão é dupla: oferecer uma mão amiga aos idosos que precisam de apoio e enriquecer a vida daqueles que desejam contribuir com seu tempo e cuidado. Acreditamos que todos têm algo especial para compartilhar, e ao conectar pessoas dispostas a ajudar com idosos que anseiam por companhia, estamos construindo uma comunidade baseada no respeito, compaixão e empatia.</p>
+
+                    </div><span class="bl-icon bl-icon-close">Fechar</span>
+                </section>
+
+                <section>
+                    <div class="bl-box">
+                        <h2>Valores</h2>
+                    </div>
+
+                    <div class="bl-content">
+
+                        <p>Somos uma ponte entre pessoas que desejam fazer a diferença e idosos que precisam de apoio. Facilitamos a conexão entre voluntários e idosos, levando em consideração seus interesses, habilidades e disponibilidade. Nossa plataforma é projetada para tornar o processo de voluntariado simples e gratificante. Desde visitas regulares para um bate-papo amigável até auxílio em tarefas do dia a dia, cada interação é uma oportunidade de criar memórias significativas.</p>
+                    </div><span class="bl-icon bl-icon-close">Fechar</span>
+                </section>
+
+                <section id="bl-work-section">
+                    <div class="bl-box">
+                        <h2>Quem Somos</h2>
+                    </div>
+
+                    <div class="bl-content">
+                        <h2>Integrantes <img src="../src/img/logo.jpg" style="
+                             max-width: 10%;
+                            border-radius: 50%;
+                            display: flex;
+                             margin-left: 10px;
+                            "></h2>
+
+
+
+                        <ul id="bl-work-items" style=" position: absolute;
+                            display: flex;
+                             top: 200px;">
+                            <li data-panel="panel-1">
+                           
+                                <a href="#"><img src="../src/img/logosemfundo.png"></a>
+                                <div class="control-panel">
+                                <span class="texto1" style="background-image: linear-gradient(#10A296, #10A296) !important;">
+                                <h3> Damião </h3>
+                                <h3>> Documentação & Frontend
+</span><h3></div>
+                                        
+                            </li>
+
+                            <li data-panel="panel-2">
+                                <a href="#"><img src="../src/img/leo2.jpg"></a>
+                                <div class="control-panel">
+                                <span class="texto1" style="background-image: linear-gradient(red,red) !important;"> <h3> Leandro </h3>
+                                <h3>>  Design, Frontend <br>& Gerenciamento de Dados</span> <h3></div>
+                            </li>
+
+                            <li data-panel="panel-3">
+                                <a href="#"><img src="../src/img/luis1.jpeg"></a>
+                                <div class="control-panel">
+
+                                <span class="texto1" style="background-image: linear-gradient(blue,blue) !important;"><h3> Luis </h3>
+                                <h3>> Frontend </span>
+                                    <h3></div>
+                            </li>
+
+
+
+                            <li data-panel="panel-4">
+                                <a href="#"><img src="../src/img/nat2.jpg"></a>
+                                <div class="control-panel">
+                                <span class="texto1" style="background-image: linear-gradient(black, black) !important;"><h3> Nathalia </h3>
+                                <h3>> Documentação &<br> Frontend 
+</span><h3></div>
+                            </li>
+
+                            <li data-panel="panel-5">
+                                <a href="#"><img src="../src/img/vinicius1.jpg"></a>
+                                <div class="control-panel">
+                                <span class="texto1"><h3> Vinicius </h3>
+                                <h3>> Backend &<br> Gerencimaneto de Dados</span>
+                                    <h3>
+</div>
+                            </li>
+
+                        </ul>
+                    </div><span class="bl-icon bl-icon-close">Fechar</span>
+                </section>
+
+                <section>
+                    <div class="bl-box">
+                        <h2> <a href="../index.php">Voltar a Pagina Principal</a></h2>
+                    </div>
+
+
+                </section>
 
             </div>
         </div>
     </div>
-</div><!-- /container -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> <script src="js/boxlayout.js"></script> <script>
-            $(function() {
-                Boxlayout.init();
-            });
-</script>
+    </div><!-- /container -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/boxlayout.js"></script>
+    <script>
+        $(function() {
+            Boxlayout.init();
+        });
+    </script>
 
 
-<script src="../src/js/quemsomos.js"></script>
-<script src="../src/js/meta-quesomos.js"></script>
+    <script src="../src/js/quemsomos.js"></script>
+    <script src="../src/js/meta-quesomos.js"></script>
 
 </body>
+
 </html>
